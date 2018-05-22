b9807a5c7fe60265272899201531197313c4538d
Mark Ormesher <m.ormesher@hotmail.ca>
2018-05-22 10:59:55 -0700

    Added main loop for getting user input

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <string>
 
 int main(){
 
-std::cout << "Hello world" << std::endl;
-return 0;
+	char inputstream;
+	std::cout << "Welcome to TourWin. Enter commands. \n";
+	do {
+		std::cout << ":";
+		std::cin >> inputstream;
+
+
+		switch (inputstream)
+		{
+			case 'q':
+				std::cout << "Good bye.\n";
+				break;
+			case 'i':
+				std::cout << "insert what? \n";
+				break;
+		} 
+	} while ('q' != inputstream);
 }
