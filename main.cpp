238f255cfe7843c9a22474500a7a545b96821052
MarkOrme <m.ormesher@hotmail.ca>
2018-05-25 09:57:04 -0700

    moved logic to business

--- main.cpp
+++ main.cpp
@@ -1,23 +1,41 @@
 #include <iostream>
 #include <string>
-
+#include <algorithm>	/* to support transform in linux enviro */ 
+#include "business.h"
+	
 int main(){
 
-	char inputstream;
-	std::cout << "Welcome to TourWin. Enter commands. \n";
-	do {
+    Business objBus;
+    
+	std::string Input;
+	std::cout << "Welcome to TourWin. Enter a command. \n";
+
+	while (true) {
 		std::cout << ":";
-		std::cin >> inputstream;
+		std::cin >> Input;
+		std::transform(Input.begin(),Input.end(),Input.begin(),::tolower);
 
-
-		switch (inputstream)
-		{
-			case 'q':
-				std::cout << "Good bye.\n";
-				break;
-			case 'i':
-				std::cout << "insert what? \n";
-				break;
-		} 
-	} while ('q' != inputstream);
+		if (Input == "new") {
+            objBus.New();
+		}
+		else if (Input == "edit"){
+            objBus.Edit();
+		}
+        else if (Input == "help"){
+            objBus.Help();
+        }
+        else if (Input == "report"){
+            objBus.Report();
+        }
+		else if (Input == "quit"){
+            objBus.Quit();
+            return 0;
+		}
+		else {
+			std::cout << "Unknown command. Type 'help' for list of valid commands.\n";
+			std::cin.clear();
+		}
+	} // end of while loop
 }
+
+
